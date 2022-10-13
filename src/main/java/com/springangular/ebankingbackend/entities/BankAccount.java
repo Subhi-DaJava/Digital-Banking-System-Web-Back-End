6230c14aa2d43a5d03e6f0ca8e936bd35c9e1db2
Subhi YD <subhy809fr@gmail.com>
2022-10-14 00:29:23 +0200

    1er commit : Initialisé projet, créé models, dto, repositories, web, enums, BankAccount et ses classes filles : SingleTable-Inheritance, h2

---
+++ src/main/java/com/springangular/ebankingbackend/entities/BankAccount.java
@@ -0,0 +1,26 @@
+package com.springangular.ebankingbackend.entities;
+
+import com.springangular.ebankingbackend.enums.AccountStatus;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "TYPE", length = 20, discriminatorType = DiscriminatorType.STRING) // length 255 par défaut et String
+@Data @NoArgsConstructor @AllArgsConstructor
+public class BankAccount {
+    @Id
+    private String id; // RIB
+    private double balance;
+    private Date createdDate;
+    private AccountStatus status;
+    @ManyToOne
+    private Customer customer;
+    @OneToMany (mappedBy = "bankAccount")
+    private List<AccountOperation> accountOperations;
+
+}
